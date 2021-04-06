0b1d6783f9dc14784efe0879cf4ebb9f144b4188
Jeff Gregory <jeffrey.s.gregory@me.com>
2021-04-06 13:15:19 -0500

    Updates to send2Bucket script adding sublocation option

--- bin/send2Bucket.sh
+++ bin/send2Bucket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function usage(){
     printf "\n$(basename $0) [OPTIONS]\n"
-    cat $0 | grep -E "[a-z]\)  # --.*$"
+    cat $0 | grep -E "[$1]\)  # --.*$"
     exit 0
 }
 
@@ -12,36 +12,43 @@ function usage(){
 
 : ${DATALOAD_S3_ROOT:?"AWS S3 root path not defined; Exiting Script"}
 
-while getopts "f:l:p:u" option; do
+while getopts "f:l:p:s:u" option; do
     case "$option" in
         f)  # -- Fully Qualified Path Name to file
             fqpn=${OPTARG}
             ;;
         l)  # -- location (CONUS, CARIB, HAWAII, etc)
-            location=${OPTARG}
+            location=${OPTARG^^}
+            ;;
+        s)  # -- sublocation (directory level below location)
+            sublocation=${OPTARG}
             ;;
         p)  # -- Project name (instrumentation, cumulus, etc)
-            project=${OPTARG}
+            # make sure it is lower case
+            project=${OPTARG,,}
             ;;
         u)  # -- Print usage message
-            usage
+            usage "a-z"
             ;;
     esac
 done
 shift $(($OPTIND - 1))
 
 [ -z $fqpn ] \
-    && printf "Filename not defined and exiting\n" \
-    && usage
+    && printf "'Filename' not defined and exiting\n" \
+    && usage "f"
 [ -z $project ] \
-    && printf "Project not defined and exiting\n" \
-    && usage
+    && printf "'Project' not defined and exiting\n" \
+    && usage "p"
 [ -z $location ] \
-    && printf "Location not defined and exiting\n" \
-    && usage
+    && printf "'Location' not defined and exiting\n" \
+    && usage "l"
+[ -z $sublocation ] \
+    && printf "'Subocation' not defined and exiting\n" \
+    && usage "s"
 
 filename=$(basename $fqpn)
-s3_destination="$DATALOAD_S3_ROOT/$project/ldm/$location/$filename"
+s3_destination="$DATALOAD_S3_ROOT/$project/ldm/$location/$sublocation/$filename"
 
 [ -z $AWS_ENDPOINT_URL ] \
     && aws s3 cp "$fqpn" "$s3_destination" \
