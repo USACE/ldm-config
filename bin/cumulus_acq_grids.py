0a1323199de3665f8f17ac2b1f87e71690960501
Jeff Gregory <jeffrey.s.gregory@me.com>
2021-06-09 11:50:26 -0500

    Cumulus acquirable grids in pqact
    Python script for gridded files picked up on ldm and processed
    with pqact.  Python 3.8 install and requirements added to the
    LDM Docker image.  The script requres a file to transfer to s3,
    an acquirable type (db acquirable) and a bucket (cwbi-data-develop
    or cwbi-data-stable).  Multiple buckets can be used.  Environment
    variables define the stable and develop buckets and URLs.

---
+++ bin/cumulus_acq_grids.py
@@ -0,0 +1,158 @@
+#!/Python-3.8.8/python
+
+import os
+import sys
+import boto3
+import botocore
+from botocore.exceptions import NoCredentialsError
+import argparse
+import logging
+import requests
+from urllib.parse import urlparse, urlunparse
+from requests.models import CaseInsensitiveDict, Response
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass
+class acquirable:
+    id: str
+    name: str
+    slug: str
+
+# Root key
+root_key = "cumulus/acquirables"
+
+# Cumulus URL
+cumulus_url = {
+    "cwbi-data-develop": os.environ.get("CUMULUS_HOST_DEVELOP"),
+    "cwbi-data-stable": os.environ.get("CUMULUS_HOST_STABLE")
+}
+
+# Create a logging object
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+sh = logging.StreamHandler()
+formatter = formatter = logging.Formatter('%(asctime)s.%(msecs)03d - ' +
+        '%(name)s:%(funcName)15s - %(levelname)-5s - %(message)s',
+        '%Y-%m-%dT%H:%M:%S')
+sh.setFormatter(formatter)
+logger.addHandler(sh)
+
+# Create the argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-p", "--fqpn", required=True, type=str, action="store", dest="fqpn"
+)
+parser.add_argument(
+    "-a", "--acquire_type", required=True, type=str, action="store", dest="acquire_type"
+)
+parser.add_argument(
+    "-b", "--bucket", required=True, action="append", dest="bucket",
+    choices=list(cumulus_url.keys())
+)
+
+def get_acquirable_id(host: str, slug: str) -> str:
+    url_parse = urlparse(host)
+    url = urlunparse((
+        url_parse.scheme,
+        url_parse.netloc,
+        "acquirables",
+        "",
+        "",
+        ""
+    ))
+
+    headers = CaseInsensitiveDict()
+    headers["content-type"] = "application/json; charset=utf-8"
+    headers["accept"] = "application/json"
+
+    with requests.Session() as s:
+        s.headers = headers
+        resp = s.get(url=url, headers=headers)
+
+    if resp.status_code == 200:
+        for acquire in resp.json():
+            _acquirable = acquirable(**acquire)
+            if _acquirable.slug == slug:
+                return _acquirable.id
+    else:
+        return None
+
+def notify_acquirablefile(api_url: str, acquirable_id: str, datetime: datetime, key: str, query: str) -> Response:
+    dtf = "%Y-%m-%dT%H:%M:%SZ"
+    url_parse = urlparse(api_url)
+    url = urlunparse((
+        url_parse.scheme,
+        url_parse.netloc,
+        "acquirablefiles",
+        "",
+        query,
+        ""
+    ))
+
+    data = {
+        "datetime": datetime.strftime(dtf),
+        "file": key, 
+        "acquirable_id": acquirable_id
+    }
+    headers = CaseInsensitiveDict()
+    headers["content-type"] = "application/json; charset=utf-8"
+    headers["accept"] = "application/json"
+
+    with requests.Session() as s:
+        s.headers = headers
+        return s.post(url=url, json=data, headers=headers)
+
+def main() -> None:
+    args = parser.parse_args()
+
+    fqpn = args.fqpn
+    filename = os.path.basename(fqpn)
+    acquire_type = args.acquire_type
+    buckets = args.bucket
+
+    if not os.path.isfile(fqpn):
+        logger.warning(f"file '{fqpn}' does not exist.")
+        logger.warning("Program exiting!")
+        sys.exit()
+
+    # Get the s3 resource and upload the file to the bucket
+    aws_s3_endpoint=os.environ.get("AWS_S3_ENDPOINT")
+    if aws_s3_endpoint:
+        s3 = boto3.resource("s3", endpoint_url=aws_s3_endpoint)
+    else:
+        s3 = boto3.resource("s3")
+
+    application_key = os.environ.get("APPLICATION_KEY")
+    for bucket in buckets:
+        key = "/".join([root_key, acquire_type, filename])
+        try:
+            s3.meta.client.upload_file(
+                fqpn,
+                bucket,
+                key
+            )
+            api_url = cumulus_url[bucket]
+            acquirable_id = get_acquirable_id(api_url, acquire_type)
+            if acquirable_id is not None:
+                resp = notify_acquirablefile(
+                    api_url=api_url,
+                    acquirable_id=acquirable_id,
+                    datetime=datetime.utcnow(),
+                    key=key,
+                    query=f"key={application_key}"
+                )
+                logger.info(f"Response status code: {resp.status_code}")
+
+        except botocore.exceptions.ParamValidationError as ex:
+            logger.warning(f"Invalid bucket name - {bucket}")
+            logger.warning("Program exiting!")
+            sys.exit()
+
+if __name__ == "__main__":
+    try:
+        main()
+    except NoCredentialsError as error:
+        logger.error(error)
+        logger.error("To run this, you must have valid credentials in "
+              "a shared credential file or set in environment variables.")
